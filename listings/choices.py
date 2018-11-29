2d7c55b54f25f4935a373819325b8996caa0dc8b
Marczu <marczu09@gmail.com>
2018-11-29 10:02:59 +0100

    Work on search querries in listings/search

---
+++ listings/choices.py
@@ -0,0 +1,45 @@
+bedroom_choices = {
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10
+}
+
+price_choices = {
+    '100000': '100,000 PLN',
+    '200000': '200,000 PLN',
+    '300000': '300,000 PLN',
+    '400000': '400,000 PLN',
+    '500000': '500,000 PLN',
+    '600000': '600,000 PLN',
+    '700000': '700,000 PLN',
+    '800000': '800,000 PLN',
+    '900000': '900,000 PLN',
+    '1000000': '$1M+',
+}
+
+province_choices = {
+    'AK': 'Alaska',
+    "D": "Dolnośląskie",
+    "C": "Kujawsko - pomorskie",
+    "L": "Lubelskie",
+    "F": "Lubuskie",
+    "E": "Lódzkie",
+    "K": "Małopolskie",
+    "W": "Mazowieckie",
+    "O": "Opolskie",
+    "R": "Podkarpackie",
+    "B": "Podlaskie",
+    "G": "Pomorskie",
+    "S": "Śląskie",
+    "T": "Świętokrzyskie",
+    "N": "Warmińsko - mazurskie",
+    "P": "Wielkopolskie",
+    "Z": "Zachodniopomorskie"
+}
--- listings/views.py
+++ listings/views.py
@@ -2,7 +2,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Listing
-
+from .choices import price_choices, bedroom_choices, province_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -13,6 +13,7 @@ def index(request):
 
     context = {
         "listings": paged_listings
+
     }
 
     return render(request, 'listings/listings.html', context)
@@ -30,4 +31,11 @@ def listing(request, listing_id):
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'province_choices': province_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+    }
+
+    return render(request, 'listings/search.html', context)
