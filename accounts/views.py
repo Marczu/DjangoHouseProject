f09bdd45832368df97d33d5164c3f173b6b2a12d
Marczu <marczu09@gmail.com>
2019-01-24 09:46:35 +0100

    small refactoring

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def register(request):
+    return render(request, 'account/register.html')
+
+def login(request):
+    return render(request, 'account/login.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request, 'account/dashboard.html')
